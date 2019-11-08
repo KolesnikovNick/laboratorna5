8cd8c3dd50d55e375c246fdfb9bb50e9d81ccce6
KolesnikovNick <57524387+KolesnikovNick@users.noreply.github.com>
2019-11-08 11:43:29 +0200

    Змінив

---
+++ ConsoleApplication26/ConsoleApplication26/ConsoleApplication26.cpp
@@ -0,0 +1,22 @@
+﻿// ConsoleApplication26.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "windows.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int y;
+	double x(2);
+	printf("Enter x:");
+	scanf_s("%f", &x);
+	do
+	{
+		y = exp(x) + fabs(x) + pow(x, 2);
+		printf("x=%f   y=%d\n", x, y);
+		x += 0.1;
+	} while (x >= 2 && x <= 3);
+	system("pause");
+	return 0;
+}
